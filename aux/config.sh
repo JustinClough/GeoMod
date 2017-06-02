9eef561739685a1aad2c78a9341538559221266f
JustinClough <clougj@rpi.edu>
2017-06-02 15:08:54 -0400

    Module setup and building scripts

---
+++ aux/config.sh
@@ -0,0 +1,9 @@
+# Configures build system using cmake
+
+source /lore/clougj/GeoMod/aux/setup.sh
+cd /lore/clougj/GeoMod/
+rm -r build/
+mkdir build
+cd /lore/clougj/GeoMod/build/
+cmake ../
+
---
+++ aux/setup.sh
@@ -0,0 +1,11 @@
+module purge
+
+module load git
+module load gdb
+module load cmake
+module load valgrind
+module load gcc/4.9.2
+module load mpich3/3.1.2-thread-multiple
+module load simmetrix/simModSuite
+module load parmetis/mpich3.1.2/4.0.3
+module load pumi
