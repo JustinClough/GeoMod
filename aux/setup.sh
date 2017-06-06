1692f8596cc31c7ed9deffaa10fa20a38f397188
JustinClough <clougj@rpi.edu>
2017-06-06 09:01:46 -0400

    Comments out simmodeler version; back to default

--- aux/setup.sh
+++ aux/setup.sh
@@ -7,5 +7,6 @@ module load valgrind
 module load gcc/4.9.2
 module load mpich3/3.1.2-thread-multiple
 module load simmetrix/simModSuite
+module load simmetrix/simModeler #/6.0-170411dev
 module load parmetis/mpich3.1.2/4.0.3
 module load pumi
