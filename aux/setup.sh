644f1ae5d0e818367cecbced07617d105ab7ebf4
JustinClough <clougj@rpi.edu>
2017-06-14 18:21:38 -0400

    Updates SimModeler version to use

--- aux/setup.sh
+++ aux/setup.sh
@@ -7,6 +7,6 @@ module load valgrind
 module load gcc/4.9.2
 module load mpich3/3.1.2-thread-multiple
 module load simmetrix/simModSuite
-module load simmetrix/simModeler #/6.0-170411dev
+module load simmetrix/simModeler/6.0-170515dev
 module load parmetis/mpich3.1.2/4.0.3
 module load pumi
