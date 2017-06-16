6f72a8ab65c4162d9cbb13d7bcd074b2e3da3b38
JustinClough <clougj@rpi.edu>
2017-06-16 17:34:09 -0400

    Splits _printer between source and header

---
+++ src/GeoMod_printer.cpp
@@ -0,0 +1,29 @@
+#include <GeoMod_printer.hpp>
+
+#include <cstdlib>
+#include <iostream>
+#include <string.h>
+
+namespace GMD
+{
+  void print_error( std::string message, bool abort_on_fail=true)
+  {
+    std::cout << "Error: " <<  message << std::endl;
+    if( abort_on_fail)
+    {
+      std::abort();
+    }
+    return;
+  }
+
+  void print_coords( double x[3])
+  {
+    std::cout << "(" ;
+    for (int i=0; i<3; i++)
+    {
+      std::cout << x[i] << ", ";
+    }
+    std::cout << "\b\b)\n";
+    return;
+  }
+}
--- src/GeoMod_printer.hpp
+++ src/GeoMod_printer.hpp
@@ -7,25 +7,8 @@
 
 namespace GMD
 {
-  void print_error( std::string message, bool abort_on_fail=true)
-  {
-    std::cout << "Error: " <<  message << std::endl;
-    if( abort_on_fail)
-    {
-      std::abort();
-    }
-    return;
-  }
+  void print_error( std::string message, bool abort_on_fail=true);
 
-  void print_coords( double x[3])
-  {
-    std::cout << "(" ;
-    for (int i=0; i<3; i++)
-    {
-      std::cout << x[i] << ", ";
-    }
-    std::cout << "\b\b)\n";
-    return;
-  }
+  void print_coords( double x[3]);
 }
 #endif
